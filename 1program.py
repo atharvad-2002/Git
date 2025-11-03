b7af833c8f16c82bcd61c60933ee328decccd3fb
Atharva Dalvi <2025.atharvad@isu.ac.in>
2025-11-03 13:13:01 +0530

    Second commit

--- 1program.py
+++ 1program.py
@@ -1,198 +1,56 @@
-# print ("Hello World")
-# print ("Atharva Dalvi")
-# name = "atharva dalvi"
-# age = 14
-# print (name)
-# print (age)
-# print ("My name is :",name)
-# print("My age is:",age)
-# print(type(name))
-# print(name,age)
-# a=25
-# b=2
-# sum=a+b
-# print(sum)
-# print(a%b)
-# print(a**b)
-# print(a==b)
-# print(a!=b)
-# a=b
-# print(a)
-# print(a==b)
-# a=100
-# a+=10
-# print(a)
-# a=10
-# b=15
-# print(not(a==b))
-# print((a==b) and (a<b))
-# print((a==b) or (a<b))
-# a=int("2")
-# print(type(a))
-# name=input("Enter your name:")
-# age=input("enter your age:")
-# print("Welcome",name)
-# print("your age is:",age)
+print ("Hello World")
+print ("Atharva Dalvi")
+name = "atharva dalvi"
+age = 14
+print (name)
+print (age)
+print ("My name is ",name)
+print("My age is ",age)
+print(type(name))
+print(name,age)
+a=25
+b=2
+sum=a+b
+print(sum)
+print(a%b)
+print(a**b)
+print(a==b)
+print(a!=b)
+a=b
+print(a)
+print(a==b)
+g=100
+g+=10
+print(g)
+y=10
+z=15
+print(not(y==z))
+print((y==z) and (y<z))
+print((y==z) or (y<z))
+q=int("2")
+print(type(q))
+nam=input("Enter your name:")
+ag=input("enter your age:")
+print("Welcome",nam)
+print("your age is:",ag)
 
 # practice question 1
-# a=int(input("Enter the number one:"))
-# b=int(input("Enter the number two:"))
-# print(a+b)
+m=int(input("Enter the number one:"))
+n=int(input("Enter the number two:"))
+print(m+n)
 
 # practice question 2
-# a=float(input("Enter the side length of square:"))
-# print("Area of square :",a*a)
+c=float(input("Enter the side length of square:"))
+print("Area of square :",c*c)
 
 # practice question 3
-# a=float(input("Enter number 1:"))
-# b=float(input("Enter the number 2:"))
+d=float(input("Enter number 1:"))
+e=float(input("Enter the number 2:"))
 
-# print("Average=",(a+b)/2)
+print("Average=",(d+e)/2)
 
 # practice question 4
-# a=float(input("Enter a:"))
-# b=float(input("Enter b:"))
-# print(a>=b)
+f=float(input("Enter a:"))
+g=float(input("Enter b:"))
+print(f>=g)
  
-# st="My name is Atharva dalvi.\nI am 18 years old"
-# print(st)
-
-# St=input("Enter Your full name: ")
-# # So=input("Enter your last name: ")
-# # print(St+ " " +So)
-# # print(len(St))
-# # print(St[0])
-# # print(St + St[-5:])
-# print(St.capitalize())
-
-# St="Atharva Dalvi"
-# print(St.replace("Dalvi","intelligent"))
-# print(St.find("th"))
-# print(St.count("a"))
-
-
-# a=int(input("Enter your marks:"))
-
-# if(a>=90):
-#     print("You have secured A grade")
-# elif(90>a>=80):
-#     print(" You have secured B grade")
-
-# elif(80>a>=70):
-#     print("You have secured C grade")
-# elif(70>a>=60):
-#     print("you have secured D grade")
-
-# else:
-#     print("You have failed")
-
-# practice question1:
-# a=int(input("Enter the numebr:"))
-# if(a%2==0):
-#     print("The entered number is a even number")
-# else:
-#     print("The number is odd")
-
-# practice question 2
-# a=float(input("Enter the first number:"))
-# b=float(input("Enter the second number:"))
-# c=float(input("Enter the third number:"))
-
-# if(a>b and a>c):
-#     print(a,"is the greatest number")
-# elif(b>a and b>c):
-#     print(b,"the greatest number")
-# else:
-#     print(c,"the greatest number")
-
-# practice question 3
-# a=int(input("Enter the number: "))
-# if(a%7==0):
-#     print("The number is a multiple of 7")
-# else:
-#     print("The number is not a multiple of 7")
-
-# Tuples and lists
-# marks=[12,13,14,15]
-# print(marks)
-# print(marks[0])
-# print(marks[3])
-# print(len(marks))
-
-# Student=["Atharva","Dalvi",95]
-# # print(Student)
-# # Student[0]="Apeksha"
-# # print(Student)
-# # print(Student[:2])
-# Student.append("Ashok")
-# print(Student)
-# # Student.sort()
-# Student.reverse()
-# print(Student)
-
-
-# practice question 1
-# movie=[]
-# movie.append(input("Enter the first movie:"))
-# movie.append(input("Enter the second movie"))
-# movie.append(input("Enter the third movie"))
-# print(movie)
-
-
-# practice question
-# Marks=['A','B','A','D','C']
-# Marks.sort()
-# print(Marks)
-
-# Dictionaries
-# main={
-#     "Name":"Atharva",
-#     "Surname":"Dalvi",
-#     "Marks":"95",
-#     "Division":"Sam Altman"
-#     }
-# print(main)
-# print(main["Name"])
-# print(main["Surname"])
-# print(main["Marks"])
-# print(main.keys())
-# arc=list(main.keys())
-# print(arc)
-
-# Sets
-# colletion={1,2,3,4}
-# print(type(colletion))
-
-# for empty set
-# collection=set()
-# print=(type(collection))
-
-# Practice question 1
-# dictionary={
-#     "table":["A peice of furniture","list of facts and figures"],
-#     "cat":"A small animal"
-#     }
-# print(type(dictionary))
-# print(dictionary)
-
-
-# practice question 2
-# sot={"java",'python',"java","C++","JvaScript","python"}
-# print(len(sot))
-
-# practice question 3
-# d2={}
-# phy=int(input("Enter the marks of physics: "))
-# d2.update({"Physics":phy})
-
-# chem=int(input("Enter the marks of chemistry: "))
-# d2.update({"Chemistry":chem})
-
-# bio=int(input("Enter the marks of biology: "))
-# d2.update({"Biology":bio})
-
-# print(d2)
-
-# practice question 4
-se={9,"9.0"}
-print(se)
